f2afad511c0a8d0c86f531e74a6f410b7cffdf34
admin <a-fedyushkin@mail.ru>
2016-09-25 23:28:39 +0300

    Часть 4 Задание 8

---
+++ ConsoleApplication14/ConsoleApplication14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i < 10000; i++)
+                if (Num(i))
+                    Console.WriteLine(i);
+
+            Console.ReadKey();
+
+        }
+
+        public static bool Num(int num)
+        {
+            return ((int)Math.Pow((num / 1000 + num % 1000 / 100 + num % 100 / 10 + num % 10), 4) == num)
+                ? true : false;
+        }
+    }
+}
