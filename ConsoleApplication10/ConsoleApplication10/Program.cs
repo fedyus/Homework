0031f38958449b50d8a1c8c522c9ef5b01b23d92
admin <a-fedyushkin@mail.ru>
2016-09-25 18:37:07 +0300

    Часть 4 Задача 6

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication38
+{
+    class Program
+    {
+
+        static int SumDel(int x)
+        {
+            int res = 0;
+            for (int i = 1; i <= x / 2; i++)
+                if (x % i == 0) res += i;
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            int n = 1;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (SumDel(SumDel(i)) == i)
+                    Console.WriteLine(i + " и " + SumDel(i) + "\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
