0328b0a0f949baed96b35c2edebbc50ae50c9976
admin <a-fedyushkin@mail.ru>
2016-09-25 18:06:01 +0300

    Часть 1 Задача 15

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, d, e, f, l;
+            l = int.Parse(Console.ReadLine());
+            a = l / 100000;
+            b = (l / 10000) % 10;
+            c = (l / 1000) % 10;
+            d = (l / 100) % 10;
+            e = (l / 10) % 10;
+            f = l % 10;
+            a += e + c;
+            d += b + f;
+            if (a == d)
+            {
+                Console.WriteLine("Билет является счастливым по-питерски");
+            }
+            else
+            {
+                Console.WriteLine("Билет не является счастливым по-питерски");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
