a4dba8254b05f8e0122f84fb9f1144bd1eb21fb8
admin <a-fedyushkin@mail.ru>
2016-09-25 17:49:36 +0300

    Часть 1 Задача 1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            x = x + y;
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("BLACK");
+            }
+            else
+            {
+                Console.WriteLine("WHITE");
+            }
+            Console.ReadLine();
+        }
+    }
+}
