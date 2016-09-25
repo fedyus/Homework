0a1d8602d035091ea5e9c1e6b758926c9b7277bc
admin <a-fedyushkin@mail.ru>
2016-09-25 18:11:38 +0300

    Часть 2 Задача 14

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, i = 0;
+            long s = 1, x, y = 0;
+            x = long.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            while (x != 0)
+            {
+                y += (x % k) * (Convert.ToInt32(Math.Pow(10, i)));
+                x /= k;
+                i++;
+            }
+            while (y != 0)
+            {
+                s *= y % 10;
+                y /= 10;
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
