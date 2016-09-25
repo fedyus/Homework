3caa1b392986f12916699de74b8a729e3b239e11
admin <a-fedyushkin@mail.ru>
2016-09-25 18:08:47 +0300

    Часть 2 Задача 12

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x2, k, x1, y2, m, y1;
+            x1 = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            x2 = Convert.ToInt32(Convert.ToString(x1), k);
+            y2 = Convert.ToInt32(Convert.ToString(y1), m);
+            Console.WriteLine(x2);
+            Console.WriteLine(y2);
+            Console.ReadLine();
+        }
+    }
+}
